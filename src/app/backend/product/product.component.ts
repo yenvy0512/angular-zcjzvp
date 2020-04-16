4ae01ea2c180a113015b49b3fc5c996ec8c45fb4
yenvy0512 <kieuyenvy0512@gmail.com>
2020-04-16 20:35:49 +0700

    1 file modified

--- src/app/backend/product/product.component.ts
+++ src/app/backend/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
+import { ProductService } from '../../product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../Product';
 

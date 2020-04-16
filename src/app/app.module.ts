fc2d2f81a2e78b6ce98351a07761aaf0ab20e548
yenvy0512 <kieuyenvy0512@gmail.com>
2020-04-16 16:15:51 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductComponent } from './backend/product.component';
 import { AdminComponent } from './backend/admin.component';
-
+import {DashboardComponent } from './backend/dashboard.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, ProductComponent, AdminComponent ],
